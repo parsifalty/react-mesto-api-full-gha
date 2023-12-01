668a3a4caf0d330d85e60846ce368d908d191cb9
Pasha Litvak <parsifalty@gmail.com>
2023-12-01 22:23:56 +0200

    psokfopd

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-const { PORT = 3000, DB_URL = "mongodb://localhost:27017/mestodb" } =
+const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
   process.env;
 const app = express();
 
