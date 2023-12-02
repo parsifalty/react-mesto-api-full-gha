c0c24ba98763fa6f1cb84d073c2c6fbaeb4aa7c4
Pasha Litvak <parsifalty@gmail.com>
2023-12-02 04:08:37 +0200

    pofskfo

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,22 @@
-const router = require('express').Router();
-const usersRouter = require('./users');
-const cardsRouter = require('./cards');
-const signupRouter = require('./signup');
-const singinRouter = require('./singin');
-const auth = require('../middlewares/auth');
+const router = require("express").Router();
+const usersRouter = require("./users");
+const cardsRouter = require("./cards");
+const signupRouter = require("./signup");
+const singinRouter = require("./singin");
+const auth = require("../middlewares/auth");
+
+router.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 
 router.use(signupRouter);
 router.use(singinRouter);
 
 router.use(auth);
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+router.use("/users", usersRouter);
+router.use("/cards", cardsRouter);
 
 module.exports = router;
